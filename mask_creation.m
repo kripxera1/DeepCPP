74a6e7534a8231685db2e56a8a206b66863544e5
none <none@none.no>
2023-06-07 14:55:10 +0200

    IMGProcessing new files

--- mask_creation.m
+++ mask_creation.m
@@ -4,10 +4,10 @@ masks.normal_mask_lambda = @(mat) cos(2*pi*mat/size(mat,1) + pi*size(mat,1)/4) .
 
 masks.log_mask_lambda = @(mat) log(mat/size(mat,1)) .* log(mat/size(mat,2));
 
-masks.first_block_lambda = @(mat) kron([1 0;0 0], mat);
-masks.second_block_lambda = @(mat) fliplr(first_block_lambda(mat));
-masks.third_block_lambda = @(mat) flipud(first_block_lambda(mat));
-masks.fourth_block_lambda = @(mat) flipud(second_block_lambda(mat));
+masks.first_block_lambda = @(mat) kron([1 0;0 0], mat*ones(size(mat,1),size(mat,2)));
+masks.second_block_lambda = @(mat) kron([0 1;0 0], mat*ones(size(mat,1),size(mat,2)));
+masks.third_block_lambda = @(mat) kron([0 0;0 1], mat*ones(size(mat,1),size(mat,2)));
+masks.fourth_block_lambda = @(mat) kron([0 0;1 0], mat*ones(size(mat,1),size(mat,2)));
 
 mat_4x4     = meshgrid(0:3,0:3);
 mat_8x8     = meshgrid(0:7,0:7);
@@ -17,11 +17,11 @@ for mask = {'normal_mask_lambda', 'log_mask_lambda', 'first_block_lambda', ...
     'second_block_lambda', 'third_block_lambda', 'fourth_block_lambda'},
     mask = cell2mat(mask);
     fprintf('Applying 4x4 matrix over %s: \n----\n',mask);
-    masks.(mask)(mat_4x4)
-    fprintf('Applying 8x8 matrix over %s: \n----\n',mask);
-    masks.(mask)(mat_8x8)
-    fprintf('Applying 16x16 matrix over %s: \n----\n',mask); 
-    masks.(mask)(mat_16x16)
+    (masks.(mask))(mat_4x4)
+#    fprintf('Applying 8x8 matrix over %s: \n----\n',mask);
+#    masks.(mask)(mat_8x8)
+#    fprintf('Applying 16x16 matrix over %s: \n----\n',mask); 
+#    masks.(mask)(mat_16x16)
     fprintf('\n\n');
 endfor
 
